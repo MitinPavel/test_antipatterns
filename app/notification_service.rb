4ee7436e217a2f48884b505470caf3e1dbc05c07
Pavel Mitin <mitin.pavel@gmail.com>
2010-09-17 23:37:11 +0300

    Code was formatted a little bit.

--- app/notification_service.rb
+++ app/notification_service.rb
@@ -1,5 +1,4 @@
-class NotificationService < 
-  Struct.new(:email_service, :sms_service, :author_repository)
+class NotificationService < Struct.new(:email_service, :sms_service, :author_repository)
   def comment_was_added(comment)
     author = author_repository.get comment.post.author_id
     email_service.deliver_new_comment_email comment, author
