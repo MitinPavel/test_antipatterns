b07c31e9f26a0395f2a28a45ec696107dce49bf3
Pavel Mitin <mitin.pavel@gmail.com>
2010-09-18 08:41:04 +0300

    The pioneer example code in the text document was improved according to the actual state.

--- spec/pioneer_spec.rb
+++ spec/pioneer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require "#{File.dirname __FILE__}/../app/notification_service.rb"
 require "#{File.dirname __FILE__}/../app/comment.rb"
 
-describe NotificationService, "#comment_was_added" do
+describe NotificationService, "#notify_about" do
   before do
    @author, @author_repository, @email_service = mock, mock, mock
    @post = mock :author_id => 123
@@ -14,7 +14,7 @@ describe NotificationService, "#comment_was_added" do
     @email_service.expects(:deliver_new_comment_email).with @comment, @author
     @sms_service.expects :deliver_new_comment_sms
 
-    notification_service.comment_was_added @comment
+    notification_service.notify_about @comment
   end
 
   it "should notify the post author by sms" do
@@ -22,7 +22,7 @@ describe NotificationService, "#comment_was_added" do
     @email_service.expects :deliver_new_comment_email
     @sms_service.expects(:deliver_new_comment_sms).with @comment, @author
 
-    notification_service.comment_was_added @comment
+    notification_service.notify_about @comment
   end
 
   def notification_service
