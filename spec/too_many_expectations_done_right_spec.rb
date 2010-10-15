5c53759f5cd67ff86ebff7738a5dbc971c9a5176
Pavel Mitin <mitin.pavel@gmail.com>
2010-10-15 06:49:31 +0300

    'too_many_expectations_done_right_spec' was refactored.

--- spec/too_many_expectations_done_right_spec.rb
+++ spec/too_many_expectations_done_right_spec.rb
@@ -4,21 +4,16 @@ require "#{File.dirname __FILE__}/../app/notification_service.rb"
 describe NotificationService, "#notify_about" do
   before do
     @sms_service, @email_service = stub_everything, stub_everything
+    @notification_service = NotificationService.new @email_service, @sms_service
   end
 
   it "should notify the post author by email" do
     @email_service.expects(:deliver_new_comment_email).with @comment, @author
-
-    notification_service.notify_about @comment
+    @notification_service.notify_about @comment
   end
 
   it "should notify the post author by sms" do
     @sms_service.expects(:deliver_new_comment_sms).with @comment, @author
-
-    notification_service.notify_about @comment
-  end
-
-  def notification_service
-    NotificationService.new @email_service, @sms_service
+    @notification_service.notify_about @comment
   end
 end
